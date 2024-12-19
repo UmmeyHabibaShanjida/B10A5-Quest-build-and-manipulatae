2773fa919a2745a1e1aac1b139995ff0d3e0002d
Habiba Shanjida <uhshanjida@gmail.com>
2024-12-20 00:19:54 +0600

    complete FAQ html file

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 document.getElementById('donate-now-btn')
     .addEventListener('click', function () {
-        
+
         const donateMoney = getInputValue('donate-input-field');
-        const donateForFloodInNoakhali = getTextValue ('donate-money-for-noakhali-flood');
-      
-        if(isNaN(donateMoney)){
-            alert('Failed to add money to donate');
+        const donateForFloodInNoakhali = getTextValue('donate-money-for-noakhali-flood');
+
+        if (isNaN(donateMoney)) {
+            alert('Failed to add money to donate.');
             return;
         }
 
-        if(donateMoney > donateAccountBalance){
-            alert('You do not have enough money to donate');
-            return;
-        }
-
-        const updateBalance = donateMoney + donateForFloodInNoakhali ;
+        const updateBalance = donateMoney + donateForFloodInNoakhali;
         document.getElementById('donate-money-for-noakhali-flood').innerText = updateBalance;
 
         const donateAccountBalance = getTextValue('donate-account-balance');
         const donateAccountBalanceUpdate = donateAccountBalance - donateMoney;
         document.getElementById('donate-account-balance').innerText = donateAccountBalanceUpdate;
-        
+
+        if (donateMoney > donateAccountBalance) {
+            alert('You do not have enough money to donate.');
+            return;
+        }
+
     })
